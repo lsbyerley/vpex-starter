6253298ced0850f45007ee1a6fca5a1f03a093d0
lsbyerley <lsbyerley@gmail.com>
2018-11-09 21:15:57 -0500

    index path

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const compression = require("compression");
+const path = require('path');
 
 const app = express();
 const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 app.use(compression());
+app.use(express.static(path.resolve('./dist')));
 
 // Use API Routes
 app.use('/api', require('./api'));
+
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 async function start() {
   // Listen the server

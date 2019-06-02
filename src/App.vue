2073e51a21061b0fd75360ee5c71270c9a0155d2
lsbyerley <lsbyerley@gmail.com>
2019-06-02 10:30:59 -0400

    convert to tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app-wrap">
+  <div class="flex flex-col min-h-screen app-wrap">
     <NavBar></NavBar>
-    <main>
-      <section class="section">
-        <div class="container container-small align-center">
-          <h3>{{ message }}</h1>
-          <p>{{ apiTest }}</p>
-          <p><a href="https://mustard-ui.com/" target="_blank">Mustard CSS</a></p>
+    <main class="flex-1">
+      <div class="py-20 hero">
+        <div class="container mx-auto px-6">
+          <h2 class="text-4xl font-bold mb-2 text-white">
+            {{ message }}
+          </h2>
+          <h3 class="text-2xl mb-8 text-gray-200">
+            {{ apiTest }}
+          </h3>
+          <button class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+            TailwindCSS
+          </button>
+        </div>
+      </div>
+      <section class="container mx-auto px-6 p-10">
+        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
+          Features
+        </h2>
+        <div class="flex items-center flex-wrap mb-20">
+          <div class="w-full md:w-1/2">
+            <h4 class="text-3xl text-gray-800 font-bold mb-3">
+              Repository Includes
+            </h4>
+            <p class="text-gray-600 mb-8">VueJS</p>
+            <p class="text-gray-600 mb-8">Parcel Bundler</p>
+            <p class="text-gray-600 mb-8">ExpressJS</p>
+            <p class="text-gray-600 mb-8">TailwindCSS</p>
+          </div>
+          <div class="w-full md:w-1/2">
+            <img src="https://www.dropbox.com/s/mimcvn6zxtoruis/health.svg?raw=1" alt="Monitoring"/>
+          </div>
         </div>
       </section>
     </main>
-    <footer></footer>
+    <FootBar></FootBar>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import NavBar from '~/src/components/NavBar'
+import FootBar from '~/src/components/FootBar'
 const apiHost = process.env.API_HOST || ''
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    FootBar
   },
   created() {
     console.log('created')
@@ -52,5 +79,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/styles/app.scss';
+.hero {
+  background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)
+}
 </style>

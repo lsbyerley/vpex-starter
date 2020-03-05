eae759c35329c930f70b743f7000ae8f0411411b
lsbyerley <lsbyerley@gmail.com>
2020-03-05 15:12:56 -0500

    bunch of layout changes for default loook

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,97 @@
   <div class="flex flex-col min-h-screen app-wrap">
     <NavBar></NavBar>
     <main class="flex-1">
-      <div class="py-20 hero">
-        <div class="container mx-auto px-6">
-          <h2 class="text-4xl font-bold mb-2 text-white">
-            {{ message }}
-          </h2>
-
-          <div v-if="!apiError">
-            <button class="bg-green-600 text-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-              {{ apiMessage }}
-            </button>
-          </div>
-          <div v-else>
-            <button class="bg-red-600 text-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-              {{ apiMessage }}
-            </button>
+      <section>
+        <div class="bg-gray-50 mx-auto max-w-screen-xl px-4 sm:px-6 pt-10 sm:pt-12 md:pt-16 lg:pt-20 xl:pt-28 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+          <div class="text-center">
+            <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              vpex starter
+              <br class="xl:hidden">
+              <span class="text-indigo-600">get goin</span>
+            </h2>
+            <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">para text</p>
+            <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+              <div class="rounded-md shadow">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                  Hero Link
+                </a>
+              </div>
+              <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                  Click here
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <section class="container mx-auto px-6 p-10">
-        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8">
-          Features
-        </h2>
-        <div class="flex items-center flex-wrap mb-20">
-          <div class="w-full md:w-1/2">
-            <h4 class="text-3xl text-gray-800 font-bold mb-3">
-              Repository Includes
-            </h4>
-            <p class="text-gray-600 mb-8">VueJS</p>
-            <p class="text-gray-600 mb-8">Parcel Bundler</p>
-            <p class="text-gray-600 mb-8">ExpressJS</p>
-            <p class="text-gray-600 mb-8">TailwindCSS</p>
+      </section>
+      <section>
+        <div class="py-12 bg-white">
+          <div class="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-screen-xl lg:px-8">
+            <div class="lg:grid lg:grid-cols-3 lg:gap-8">
+              <div>
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
+                  </svg>
+                </div>
+                <div class="mt-5">
+                  <h5 class="text-lg leading-6 font-medium text-gray-900">VueJS</h5>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                  </p>
+                </div>
+              </div>
+              <div class="mt-10 lg:mt-0">
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
+                  </svg>
+                </div>
+                <div class="mt-5">
+                  <h5 class="text-lg leading-6 font-medium text-gray-900">Parcel Bundler</h5>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                  </p>
+                </div>
+              </div>
+              <div class="mt-10 lg:mt-0">
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/>
+                  </svg>
+                </div>
+                <div class="mt-5">
+                  <h5 class="text-lg leading-6 font-medium text-gray-900">TailwindCSS</h5>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="w-full md:w-1/2">
-            <img src="https://www.dropbox.com/s/mimcvn6zxtoruis/health.svg?raw=1" alt="Monitoring"/>
+        </div>
+      </section>
+      <section>
+        <div class="bg-gray-50">
+          <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+              Ready to dive in?
+              <br />
+              <span class="text-indigo-600">Start your free trial today.</span>
+            </h2>
+            <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+              <div class="inline-flex rounded-md shadow">
+                <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                  Get started
+                </a>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                  Learn more
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,97 @@
 <template>
-  <nav>
-      <div class="container mx-auto px-6 py-2 flex justify-between items-center">
-        <a class="font-bold text-2xl lg:text-4xl" href="#">VPEX</a>
-        <div class="block lg:hidden">
-          <button class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+  <nav class="bg-gray-800">
+
+    <div class="desktop-nav max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex-shrink-0 text-gray-400">
+            <!-- The Logo --->
+            <!--<img class="block lg:hidden h-8 w-auto" src="/img/logos/workflow-mark-on-dark.svg" alt="" />
+            <img class="hidden lg:block h-8 w-auto" src="/img/logos/workflow-logo-on-dark.svg" alt="" />-->
+            <p>your logo</p>
+          </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex">
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+            </div>
+          </div>
+        </div>
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex items-center">
+            <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+              </svg>
+            </button>
+            <div class="ml-3 relative">
+              <div>
+                <button @click="profileOpen = !profileOpen" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
+                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                </button>
+              </div>
+              <transition enter-active-class="transition ease-out duration-100" enter-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <div v-show="profileOpen" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                  <div class="py-1 rounded-md bg-white shadow-xs">
+                    <a href="#" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Your Profile</a>
+                    <a href="#" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Hello1</a>
+                    <a href="#" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Sign out</a>
+                  </div>
+                </div>
+              </transition>
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 flex sm:hidden">
+          <button @click="navOpen = !navOpen" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path :class="{'hidden': navOpen, 'inline-flex': !navOpen }" class="" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              <path :class="{'hidden': !navOpen, 'inline-flex': navOpen }" class="" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
-        <div class="hidden lg:block">
-          <ul class="inline-flex">
-            <li>
-              <a class="px-4 font-bold" href="/">Home</a>
-            </li>
-            <li>
-              <a class="px-4 hover:text-gray-800" href="#">About</a>
-            </li>
-          </ul>
+      </div>
+    </div>
+
+    <div :class="{'block': navOpen, 'hidden': !navOpen}" class="mobile-nav sm:hidden">
+      <div class="px-2 pt-2 pb-3">
+        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
+        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
+        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
+        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+      </div>
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium leading-6 text-white">Tom Cook</div>
+            <div class="text-sm font-medium leading-5 text-gray-400">tom@example.com</div>
+          </div>
+        </div>
+        <div class="mt-3 px-2">
+          <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Your Profile</a>
+          <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Hello2</a>
+          <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Sign out</a>
         </div>
       </div>
-    </nav>
+    </div>
+
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  data() {
+    return {
+      profileOpen: false,
+      navOpen: false
+    }
+  }
 }
 </script>
 
